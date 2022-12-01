0faf1ec3a401da051a6d37dcafcd3f234df3e1d6
thibault-cne <thibault.chene23@gmail.com>
2022-12-01 21:46:43 +0100

    [+] v-0.0.1 day-01

---
+++ day-01/src/main.rs
@@ -0,0 +1,48 @@
+// ************************************************************************** \\
+//                                                                            \\
+//                                                                            \\
+//   main.rs                                                                  \\
+//                                                                            \\
+//   By: Thibault Cheneviere <thibault.cheneviere@telecomnancy.eu>            \\
+//                                                                            \\
+//   Created: 2022/12/01 13:09:24 by Thibault Cheneviere                      \\
+//   Updated: 2022/12/01 13:40:10 by Thibault Cheneviere                      \\
+//                                                                            \\
+// ************************************************************************** \\
+
+use std::fs;
+
+fn main() {
+	let file_path = "./input.txt";
+
+	println!("In file {}", file_path);
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+	let split: Vec<&str> = contents.split('\n').collect();
+	let mut max_cal: Vec<u32> = Vec::new();
+	max_cal.push(0);
+	max_cal.push(0);
+	max_cal.push(0);
+	let mut current_cal: u32 = 0;
+
+	for x in split {
+		match x {
+			"" => {
+				if current_cal > max_cal[0] {
+					max_cal.insert(0, current_cal);
+				} else if current_cal > max_cal[1] {
+					max_cal.insert(1, current_cal);
+				} else if current_cal > max_cal[2] {
+					max_cal.insert(2, current_cal);
+				}
+				current_cal = 0;
+			}
+			_ => current_cal += x.parse::<u32>().unwrap(),
+		}
+
+	}
+
+    println!("With text:\n{}", max_cal[0] + max_cal[1] + max_cal[2]);
+}
