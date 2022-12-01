7204303574a61dedf39db96ef6dd5167be337d5f
thibault-cne <thibault.chene23@gmail.com>
2022-12-02 00:29:02 +0100

    [+] optimize if statements

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -6,7 +6,7 @@
 //   By: Thibault Cheneviere <thibault.cheneviere@telecomnancy.eu>            \\
 //                                                                            \\
 //   Created: 2022/12/01 13:09:24 by Thibault Cheneviere                      \\
-//   Updated: 2022/12/01 13:40:10 by Thibault Cheneviere                      \\
+//   Updated: 2022/12/02 00:28:17 by Thibault Cheneviere                      \\
 //                                                                            \\
 // ************************************************************************** \\
 
@@ -26,17 +26,23 @@ fn main() {
 	max_cal.push(0);
 	max_cal.push(0);
 	let mut current_cal: u32 = 0;
+	let mut is_insrt: bool = false;
 
 	for x in split {
 		match x {
 			"" => {
 				if current_cal > max_cal[0] {
 					max_cal.insert(0, current_cal);
-				} else if current_cal > max_cal[1] {
-					max_cal.insert(1, current_cal);
-				} else if current_cal > max_cal[2] {
-					max_cal.insert(2, current_cal);
+					is_insrt = true;
 				}
+				if !is_insrt && current_cal > max_cal[1] {
+					max_cal.insert(1, current_cal);
+				}
+				if !is_insrt &&  current_cal > max_cal[2] {
+					max_cal[2] = current_cal;
+				}
+
+				is_insrt = false;
 				current_cal = 0;
 			}
 			_ => current_cal += x.parse::<u32>().unwrap(),
