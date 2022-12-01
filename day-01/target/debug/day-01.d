bf3564bd85fce2c591d631bbde59ab8703e5a96f
thibault-cne <thibault.chene23@gmail.com>
2022-12-01 21:48:08 +0100

    [-] remove target

--- day-01/target/debug/day-01.d
+++
@@ -1,1 +0,0 @@
-/Users/thibault/Documents/Perso/Rust/advent-of-code-r22/day-01/target/debug/day-01: /Users/thibault/Documents/Perso/Rust/advent-of-code-r22/day-01/src/main.rs
--- day-01/target/debug/deps/day_01-e665e7b71c3b3c80.d
+++
@@ -1,5 +0,0 @@
-/Users/thibault/Documents/Perso/Rust/advent-of-code-r22/day-01/target/debug/deps/day_01-e665e7b71c3b3c80: src/main.rs
-
-/Users/thibault/Documents/Perso/Rust/advent-of-code-r22/day-01/target/debug/deps/day_01-e665e7b71c3b3c80.d: src/main.rs
-
-src/main.rs:
