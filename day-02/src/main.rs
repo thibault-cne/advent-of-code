16164997d90ddda44d91a3375b39a92c1135e332
thibault-cne <thibault.chene23@gmail.com>
2022-12-02 11:08:31 +0100

    [+] complete day 2

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -6,7 +6,7 @@
 //   By: Thibault Cheneviere <thibault.cheneviere@telecomnancy.eu>            \\
 //                                                                            \\
 //   Created: 2022/12/02 10:42:14 by Thibault Cheneviere                      \\
-//   Updated: 2022/12/02 10:52:19 by Thibault Cheneviere                      \\
+//   Updated: 2022/12/02 11:06:47 by Thibault Cheneviere                      \\
 //                                                                            \\
 // ************************************************************************** \\
 
@@ -25,34 +25,47 @@ fn main() {
 	for x in split {
 		let decisions: Vec<&str> = x.split(' ').collect();
 
-		match decisions[0] {
-			"A" => {
-				match decisions[1] {
-					"X" => result += 1 + 3,
-					"Y" => result += 2 + 6,
-					"Z" => result += 3,
-					_ => (),
-				}
-			}
-			"B" => {
-				match decisions[1] {
-					"X" => result += 1 + 0,
-					"Y" => result += 2 + 3,
-					"Z" => result += 3 + 6,
-					_ => (),
-				}
-			}
-			"C" => {
-				match decisions[1] {
-					"X" => result += 1 + 6,
-					"Y" => result += 2 + 0,
-					"Z" => result += 3 + 3,
-					_ => (),
-				}
-			}
+		if decisions.len() != 2 {
+			break;
+		}
+
+		match decisions[1] {
+			"X" => result += decider(decisions[0], decisions[1]) + 0,
+			"Y" => result += decider(decisions[0], decisions[1]) + 3,
+			"Z" => result += decider(decisions[0], decisions[1]) + 6,
 			_ => (),
 		}
 	}
 
 	println!("Result {}", result);
+}
+
+fn decider(elf: &str, result: &str) -> u32 {
+	match result {
+		"X" => {
+			match elf {
+				"A" => 3,
+				"B" => 1,
+				"C" => 2,
+				_ => 0,
+			}
+		}
+		"Y" => {
+			match elf {
+				"A" => 1,
+				"B" => 2,
+				"C" => 3,
+				_ => 0,
+			}
+		}
+		"Z" => {
+			match elf {
+				"A" => 2,
+				"B" => 3,
+				"C" => 1,
+				_ => 0,
+			}
+		}
+		_ => 0,
+	}
 }
