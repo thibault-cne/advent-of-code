ba9a4a547fb85aed0adc12fdbd7b8cd001826e76
thibault-cne <thibault.chene23@gmail.com>
2022-12-05 15:32:13 +0100

    [+] fix spacing

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -6,7 +6,7 @@
 //   By: Thibault Cheneviere <thibault.cheneviere@telecomnancy.eu>            \\
 //                                                                            \\
 //   Created: 2022/12/01 13:09:24 by Thibault Cheneviere                      \\
-//   Updated: 2022/12/05 15:24:46 by Thibault Cheneviere                      \\
+//   Updated: 2022/12/05 15:32:06 by Thibault Cheneviere                      \\
 //                                                                            \\
 // ************************************************************************** \\
 
@@ -14,6 +14,6 @@ use day_01::{part_one, part_two};
 
 fn main() {
 	println!("--2021 day 01 solution--");
-    println!("Part one: {}", part_one("./input.txt"));
-    println!("Part two: {}", part_two("./input.txt"));
+	println!("Part one: {}", part_one("./input.txt"));
+	println!("Part two: {}", part_two("./input.txt"));
 }
