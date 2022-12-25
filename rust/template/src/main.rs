4d40e511e3ec4bde6f6d33675b6e3b4ef4871ba6
thibault-cne <thibault.chene23@gmail.com>
2022-12-25 17:00:43 +0100

    [+] add rust template

---
+++ rust/template/src/main.rs
@@ -0,0 +1,7 @@
+use {{project-name}}::{part_one, part_two};
+
+fn main() {
+	println!("--2022 day {{day-number}} solution --");
+	println!("Part one: {}", part_one("input.txt"));
+	println!("Part two: {}", part_two("input.txt"));
+}
